fa39b3c35a21fe3f7d1ea8edab418e64b9041065
gian <gianfranco.jsx@gmail.com>
2022-07-10 18:45:14 -0300

    eslint fix

--- src/theme.js
+++ src/theme.js
@@ -1,4 +1,4 @@
-import styled, { createGlobalStyle, css } from 'styled-components';
+import { createGlobalStyle, css } from 'styled-components';
 import { Title, Description } from './App.styles';
 
 const useTypingTextStyles = css`
